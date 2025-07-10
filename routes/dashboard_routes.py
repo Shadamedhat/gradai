2a6ee1b0cc3b304edd08bd9a49a682a528a88a5d
Shadamedhat <shadamedhat12@gmail.com>
2025-07-10 03:24:29 +0300

    ✅ Register dashboard blueprint and fix feedback_analysis route

--- routes/dashboard_routes.py
+++ routes/dashboard_routes.py
@@ -14,3 +14,5 @@ dashboard_bp.route('/top_active_students', methods=['GET'])(get_top_active_stude
 dashboard_bp.route('/ratings_distribution', methods=['GET'])(get_ratings_distribution)
 dashboard_bp.route('/appointments_status', methods=['GET'])(get_appointments_status)
 dashboard_bp.route('/treatment_orders_by_month', methods=['GET'])(get_treatment_orders_by_month)
+dashboard_bp.route('/treatment_forecast', methods=['GET'])(treatment_forecast)  # ✅ توقع عدد الحالات
+dashboard_bp.route('/feedback_analysis', methods=['GET'])(analyze_feedback)
